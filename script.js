89214e3868bdc9b0b8a7af65168701e84003e7db
ferniemann <hello@ferdinand-niemann.eu>
2021-10-22 15:11:33 +0200

    setup: General template for the todo app task

--- script.js
+++ script.js
@@ -1,173 +1,157 @@
-class TodoItem {
-    constructor(todoDescription) {
-        this.description = todoDescription
-        this.done = false
+const url = "http://localhost:4730/todos"
+const btnAdd = document.getElementById("btn-todo")
+const filterButtons = document.querySelectorAll("#filter-mask input")
+
+btnAdd.addEventListener("click", addTodo)
+filterButtons.forEach(button => {
+    button.addEventListener("click", filterTodos)
+})
+
+getData()
+
+function renderData(todos) {
+    const list = document.getElementById("todo-list")
+
+    list.innerHTML = ""
+
+    todos.forEach(todo => {
+        const liEl = document.createElement("li")
+        const todoText = todo.description
+
+        liEl.setAttribute("data-id", todo.id)
+        liEl.addEventListener("click", changeTodoState)
+
+        const checkbox = document.createElement("input")
+        checkbox.type = "checkbox"
+        checkbox.checked = todo.done
+        checkbox.id = createId(todoText)
+
+        const label = document.createElement("label")
+        label.setAttribute("for", checkbox.id)
+        label.innerText = todoText
+
+        const button = document.createElement("button")
+        button.classList.add("btn-delete")
+        button.innerText = "X"
+        button.addEventListener("click", deleteTodo)
+
+        liEl.append(checkbox, label, button)
+        list.appendChild(liEl)
+    })
+}
+
+function getData() {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => renderData(data))
+}
+
+function addTodo() {
+    const textField = document.getElementById("input-todo")
+    const todoText = textField.value
+    const newTodo = {
+        description: todoText,
+        done: false
+    }
+
+    if (!checkForDuplicate(todoText) && todoText !== "") {
+        addData(newTodo)
+    } else {
+        alert("Dieser Eintrag ist bereits vorhanden oder leer")
+    }
+    textField.value = ""
+}
+
+function addData(data) {
+    const requestOptions = {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(data)
+    }
+
+    fetch(url, requestOptions)
+    .then(res => res.json())
+    .then(() => getData())
+}
+
+function checkForDuplicate(newTodo) {
+    newTodo = createId(newTodo)
+    const allTodos = document.querySelectorAll("input[type='checkbox']")
+
+    for (let i = 0; i < allTodos.length; i++) {
+        const currentTodo = allTodos[i].id
+
+        if (currentTodo === newTodo) {
+            return true
+        }
     }
 }
 
-class TodoApp {
-    url = "http://localhost:4730/todos"
+function changeTodoState(e) {
+    const target = e.target
+    const todoId = target.getAttribute("data-id")
+    const checkbox = target.querySelector("input[type='checkbox']")
+    checkbox.checked = !checkbox.checked
 
-    constructor() {
-        this.getData()
+    updateData(todoId, checkbox.checked)
+}
 
-        document.getElementById("btn-todo").addEventListener("click", () => this.addTodo())
+function updateData(id, state) {
+    const requestOptions = {
+        method: "PATCH",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({done: state})
     }
 
-    renderData(todos) {
-        const list = document.getElementById("todo-list")
+    fetch(url + `/${id}`, requestOptions)
+    .then(res => res.json())
+    .then(() => getData())
+}
 
-        list.innerHTML = ""
+function deleteTodo(e) {
+    e.stopPropagation()
+    const todoId = e.target.parentElement.getAttribute("data-id")
+    deleteData(todoId)
+}
 
+function deleteData(id) {
+    fetch(url + `/${id}`, {method: "DELETE"})
+    .then(res => res.json())
+    .then(() => getData())
+}
+
+function filterTodos(e) {
+    const btnFilter = e.target
+    const btnId = btnFilter.id
+    const todos = document.querySelectorAll("input[type='checkbox']")
+
+    if(btnId === "filter-all") {
         todos.forEach(todo => {
-            const liEl = document.createElement("li")
-            const todoText = todo.description
-
-            liEl.setAttribute("data-id", todo.id)
-
-            const checkbox = document.createElement("input")
-            checkbox.type = "checkbox"
-            checkbox.checked = todo.done
-            checkbox.id = this.createId(todoText)
-
-            const label = document.createElement("label")
-            label.setAttribute("for", checkbox.id)
-            label.innerText = todoText
-
-            const button = document.createElement("button")
-            button.classList.add("btn-delete")
-            button.innerText = "X"
-
-            liEl.append(checkbox, label, button)
-            list.appendChild(liEl)
+            todo.parentElement.hidden = false
         })
     }
 
-    eventHandler() {
-        const liElements = document.querySelectorAll("ul li")
-        const deleteButtons = document.querySelectorAll(".btn-delete")
-        const filter = document.getElementById("filter-mask")
-
-        liElements.forEach(li => li.addEventListener("click", (e) => this.changeTodoState(e)))
-
-        deleteButtons.forEach(button => button.addEventListener("click", (e) => this.deleteTodo(e), false))
-
-        filter.addEventListener("change", this.filterTodos)
-    }
-
-    getData() {
-        fetch(this.url)
-        .then(res => res.json())
-        .then(data => this.renderData(data))
-        .then(() => this.eventHandler())
-    }
-
-    addTodo() {
-        const textField = document.getElementById("input-todo")
-        const todoText = textField.value
-        const newTodo = new TodoItem(todoText)
-
-        if (!this.checkForDuplicate(todoText) && todoText !== "") {
-            this.addData(newTodo)
-        } else {
-            alert("Dieser Eintrag ist bereits vorhanden oder leer")
-        }
-        textField.value = ""
-    }
-
-    addData(data) {
-        const requestOptions = {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(data)
-        }
-
-        fetch(this.url, requestOptions)
-        .then(res => res.json())
-        .then(() => this.getData())
-    }
-
-    checkForDuplicate(newTodo) {
-        newTodo = this.createId(newTodo)
-        const allTodos = document.querySelectorAll("input[type='checkbox']")
-
-        for (let i = 0; i < allTodos.length; i++) {
-            const currentTodo = allTodos[i].id
-
-            if (currentTodo === newTodo) {
-                return true
-            }
-        }
-    }
-
-    changeTodoState(e) {
-        const target = e.target
-        const todoId = target.getAttribute("data-id")
-        const checkbox = target.querySelector("input[type='checkbox']")
-        checkbox.checked = !checkbox.checked
-
-        this.updateData(todoId, checkbox.checked)
-    }
-
-    updateData(id, state) {
-        const requestOptions = {
-            method: "PATCH",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({done: state})
-        }
-
-        fetch(this.url + `/${id}`, requestOptions)
-        .then(res => res.json())
-        .then(() => this.getData())
-    }
-
-    deleteTodo(e) {
-        e.stopPropagation()
-        const todoId = e.target.parentElement.getAttribute("data-id")
-        this.deleteData(todoId)
-    }
-
-    deleteData(id) {
-        fetch(this.url + `/${id}`, {method: "DELETE"})
-        .then(res => res.json())
-        .then(() => this.getData())
-    }
-
-    filterTodos(e) {
-        const btnFilter = e.target
-        const btnId = btnFilter.id
-        const todos = document.querySelectorAll("input[type='checkbox']")
-
-        if(btnId === "filter-all") {
-            todos.forEach(todo => {
+    if(btnId === "filter-done") {
+        todos.forEach(todo => {
+            if (todo.checked === false) {
+                todo.parentElement.hidden = true
+            } else {
                 todo.parentElement.hidden = false
-            })
-        }
-
-        if(btnId === "filter-done") {
-            todos.forEach(todo => {
-                if (todo.checked === false) {
-                    todo.parentElement.hidden = true
-                } else {
-                    todo.parentElement.hidden = false
-                }
-            })
-        }
-
-        if(btnId === "filter-open") {
-            todos.forEach(todo => {
-                if (todo.checked === true) {
-                    todo.parentElement.hidden = true
-                } else {
-                    todo.parentElement.hidden = false
-                }
-            })
-        }
+            }
+        })
     }
 
-    createId(string) {
-        return string.trim().replaceAll(" ", "").toLowerCase()
+    if(btnId === "filter-open") {
+        todos.forEach(todo => {
+            if (todo.checked === true) {
+                todo.parentElement.hidden = true
+            } else {
+                todo.parentElement.hidden = false
+            }
+        })
     }
 }
 
-new TodoApp()
+function createId(string) {
+    return string.trim().replaceAll(" ", "").toLowerCase()
+}
